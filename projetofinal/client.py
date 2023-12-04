d534bd60413540fcc09bc5bdf05eec657b07e645
Artur-Cesar16 <144617098+Artur-Cesar16@users.noreply.github.com>
2023-12-03 23:00:53 -0300

    Update client.py

--- projetofinal/client.py
+++ projetofinal/client.py
@@ -1,5 +1,10 @@
 import socket
-from socket_constants import *
+
+HOST_SERVER = "localhost"
+SOCKET_PORT = 5050
+MAX_LISTEN = 5
+BUFFER_SIZE = 2048
+CODE_PAGE = 'utf-8'
 
 # Digitando a mensagem a ser enviada
 mensagem = input('Digite a mensagem: ')
