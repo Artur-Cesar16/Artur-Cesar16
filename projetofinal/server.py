0dd05373824b158be8d18e0f4d2b05e1067b0279
Artur-Cesar16 <144617098+Artur-Cesar16@users.noreply.github.com>
2023-12-03 23:04:56 -0300

    Update server.py

--- projetofinal/server.py
+++ projetofinal/server.py
@@ -1,10 +1,5 @@
 import socket
-
-HOST_SERVER = "localhost"
-SOCKET_PORT = 5050
-MAX_LISTEN = 5
-BUFFER_SIZE = 2048
-CODE_PAGE = 'utf-8'
+from socket_constants import *
 
 print('Recebendo Mensagens...\n\n')
 
